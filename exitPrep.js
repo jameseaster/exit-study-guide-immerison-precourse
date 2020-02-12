e54bdb985b0fdbfd3e98130da49f3b2b1a25331f
James Easter <james.m.easter@gmail.com>
2020-02-12 17:12:35 -0600

    Pass all 10 problems

--- exitPrep.js
+++ exitPrep.js
@@ -15,14 +15,31 @@
 
 const pureShuffle = array => {
     // your code here
+    const arrayCopy = [...array];
+    const shuffled = [];
+
+    while(arrayCopy.length){
+        let num = Math.floor(Math.random()*arrayCopy.length)
+        shuffled.push(arrayCopy[num]);
+        arrayCopy.splice(num, 1);
+    }
+
+    return shuffled;
 };
 
 var isPalindrome = (string) => {
     // your code here
+    if(string === '') return undefined;
+    if(string.length <= 1) return true;
+    if(string[0].toLowerCase() === string[string.length-1].toLowerCase()){
+        return isPalindrome(string.slice(1, string.length-1));
+    }
+    return false;
 }
 
-const mergeObjects = obj => {
+const mergeObjects = (obj, ...args) => {
     // your code here
+    return Object.assign(obj, ...args);
 };
 
 
@@ -33,15 +50,32 @@ const mergeObjects = obj => {
 
 var replaceValuesInObj = (obj, value, newValue) => {
     // your code here
+    Object.keys(obj).forEach(key=>{
+        if(obj[key] === value) obj[key] = newValue;
+        if(typeof obj[key] === 'object') replaceValuesInObj(obj[key], value, newValue)
+    })
+    return obj;
 };
 
 var addKeysToExistingObj = (obj, newKey, newValue) => {
     // your code here
+    obj[newKey] = newValue;
+
+    Object.keys(obj).forEach(key =>{
+        if(typeof obj[key] === 'object') addKeysToExistingObj(obj[key], newKey, newValue)
+    })
+
+    return obj;
 };
 
 var map = (arr, func) => {
     // your code here
+    if(!arr.length) return [];
+    let mappedArr = [];
+    mappedArr.push(func(arr[0]));
+    return mappedArr.concat(map(arr.slice(1), func));
 }
+
 
 
 /////////////////////////////////////////////////////////////////
@@ -64,12 +98,20 @@ var comedians = [
 /* Solve by chaining native methods of map and filter only */
 var comediansFilteredAndMapped = (comedians) => {
     // Your code here
-
+    return comedians.filter(obj=>obj.begin >= 2005 && obj.actor.length > 10)
+        .map(comedian=> {
+            return  {
+                        appearanceNumber: `#${comedian.number}`,
+                        name: comedian.actor,
+                        seasonsActive: comedian.end - comedian.begin + 1
+                    }
+        })
 };
 
 var comedianNamesFilteredAndMapped = (comedians) => {
     // Your code here
-
+    return comedians.filter(obj=>obj.begin >= 2005 && obj.actor.length > 10)
+        .map(comedian=> comedian.actor);
 };
 
 
@@ -78,12 +120,25 @@ var comedianNamesFilteredAndMapped = (comedians) => {
 /* Solve by using native method of reduce only */
 var comediansReduced1 = (comedians) => {
     // Your code here
+    return comedians.reduce((arr, cur)=>{
+        if(cur.begin>=2005 && cur.actor.length>10) {
+            arr = arr.concat({
+                appearanceNumber: `#${cur.number}`,
+                name: cur.actor,
+                seasonsActive: cur.end - cur.begin + 1
+            })
+        }
+        return arr;
+    }, [])
 
 };
 
 var comediansReduced2 = (comedians) => {
     // Your code here
-
+    return comedians.reduce((arr, cur)=>{
+        if(cur.begin>=2005 && cur.actor.length>10) arr = arr.concat(cur.actor);
+        return arr;
+    }, [])
 };
 
 
